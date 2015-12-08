6d11b25b5f946698e1fca772b245b50b3a3202e4
gideongrossman <gideon@velocitek.com>
2015-12-07 17:22:57 -1000

    change a view

--- turtle_shell/views.py
+++ turtle_shell/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the turtle_shell index.")
     
 def person(request):
-    return HttpResponse("You're on the person page now.")
+    return render(request, 'turtle_shell/people_index.html')
