8522141adf7e238b53644e8b2be613d4d74f69e5
gideongrossman <gideon@velocitek.com>
2015-12-07 17:00:32 -1000

    make person model and add app to settings installed_apps

--- turtle_shell/models.py
+++ turtle_shell/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    person_name = models.CharField(max_length=200)
+    document = models.FileField(upload_to='documents')
+    def __str__(self):
+        return self.question_text
--- turtle_shell/views.py
+++ turtle_shell/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the turtle_shell index.")
+    
+def person(request):
+    return HttpResponse("You're on the person page now.")
